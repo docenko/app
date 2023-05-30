52f89467db825692b8a1105e8f1feb0663ad3262
docenko <docenko.andre@mail.ru>
2023-05-30 23:15:29 +0300

    Start game

---
+++ alien_invasion.py
@@ -0,0 +1,12 @@
+import sys
+
+import pygame
+
+class AlienInnvasin:
+    """Класс дял управления ресурсами и поведением игры"""
+    
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
+        pagame.init()
+        
+        
