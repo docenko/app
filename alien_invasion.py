070c3457a5b1ebf840fbcc6df4dbd692ebe39f14
docenko <docenko.andre@mail.ru>
2023-06-19 23:48:54 +0300

    Создание класса Settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,33 @@ import sys
 
 import pygame
 
-class AlienInnvasin:
+class AlienInvasion:
     """Класс дял управления ресурсами и поведением игры"""
     
     def __init__(self):
         """Инициализирует игру и создает игровые ресурсы"""
-        pagame.init()
+        pygame.init()
         
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
         
+        #Назначение цвета фона
+        self.bg_color = (230, 230, 230)
+        
+    def run_game(self):
+        """Запуск основоного цикла инры"""
+        while True:
+            #Отслеживание событий клавиатуры
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            #При каждом проходе цикла перерисовывается экран
+            self.screen.fill(self.bg_color)        
+            #Отображение последнего проресованного экрана
+            pygame.display.flip()
+            
+if __name__ == '__main__':
+    #Создание экземплара и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()
